e17ab6fd23c83cc98e23c1ba849dce1b7197bcca
Foad Botan <foadbotan@gmail.com>
2022-09-29 22:42:22 +1000

    Create updateTile

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,10 +5,22 @@ export default function Board() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [currentPlayer, setCurrentPlayer] = useState("X");
 
+  function updateTile(index) {
+    if (board[index]) return;
+    setBoard((prevBoard) => prevBoard.map((value, i) => (i === index ? currentPlayer : value)));
+    setCurrentPlayer((prevPlayer) => (prevPlayer === "X" ? "O" : "X"));
+  }
+
   return (
     <div className="board">
       {board.map((value, index) => (
-        <Tile key={index} value={value} index={index} currentPlayer={currentPlayer} />
+        <Tile
+          key={index}
+          value={value}
+          index={index}
+          currentPlayer={currentPlayer}
+          updateTile={updateTile}
+        />
       ))}
     </div>
   );
