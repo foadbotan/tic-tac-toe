7c70628003d42b30bbd8576f1eb8783fba714b0f
Foad Botan <foadbotan@gmail.com>
2022-09-29 22:43:46 +1000

    Add initial Tile code and hover css

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,3 +1,15 @@
-export default function Tile() {
-  return <div className="tile">X</div>;
+import useHover from "../hooks/useHover";
+
+export default function Tile({ value, index, currentPlayer, updateTile }) {
+  const [ref, isHovered] = useHover();
+
+  function handleClick() {
+    updateTile(index);
+  }
+
+  return (
+    <div ref={ref} onClick={handleClick} className={`tile ${isHovered && !value ? "hover" : ""}`}>
+      {value ? value : isHovered && currentPlayer}
+    </div>
+  );
 }
