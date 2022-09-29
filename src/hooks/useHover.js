d5e17b81ca5e0c7334638bdec43b75144497b19b
Foad Botan <foadbotan@gmail.com>
2022-09-29 22:39:25 +1000

    Create useHover hook

---
+++ src/hooks/useHover.js
@@ -0,0 +1,23 @@
+import { useState, useEffect, useRef } from "react";
+
+export default function useHover() {
+  const [isHovered, setIsHovered] = useState(false);
+  const ref = useRef(null);
+
+  const handleMouseOver = () => setIsHovered(true);
+  const handleMouseOut = () => setIsHovered(false);
+
+  useEffect(() => {
+    const node = ref.current;
+    if (node) {
+      node.addEventListener("mouseover", handleMouseOver);
+      node.addEventListener("mouseout", handleMouseOut);
+      return () => {
+        node.removeEventListener("mouseover", handleMouseOver);
+        node.removeEventListener("mouseout", handleMouseOut);
+      };
+    }
+  }, [ref.current]);
+
+  return [ref, isHovered];
+}
