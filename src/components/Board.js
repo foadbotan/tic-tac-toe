6c895b870f9ee00ac9e0da7d2f3479da4454d13e
Foad Botan <foadbotan@gmail.com>
2022-09-29 22:41:42 +1000

    Create currentPlayer state

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,12 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Tile from "./Tile";
 
 export default function Board() {
   const [board, setBoard] = useState(Array(9).fill(null));
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+
   return (
     <div className="board">
       {board.map((value, index) => (
-        <Tile key={index} value={value} />
+        <Tile key={index} value={value} index={index} currentPlayer={currentPlayer} />
       ))}
     </div>
   );
