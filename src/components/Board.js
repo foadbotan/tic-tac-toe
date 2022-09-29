963f9699fe53b7bfe0660541bdc329c96515af4a
Foad Botan <foadbotan@gmail.com>
2022-09-29 23:29:09 +1000

    Check for winner or draw

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,12 +4,56 @@ import Tile from "./Tile";
 export default function Board() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [winner, setWinner] = useState(null);
 
   function updateTile(index) {
     if (board[index]) return;
     setBoard((prevBoard) => prevBoard.map((value, i) => (i === index ? currentPlayer : value)));
     setCurrentPlayer((prevPlayer) => (prevPlayer === "X" ? "O" : "X"));
   }
+
+  function checkHasWinner() {
+    const winningCombinations = [
+      // Horizontal
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      // Vertical
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      // Diagonal
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    winningCombinations.forEach(([a, b, c]) => {
+      const combination = board[a] + board[b] + board[c];
+      if (combination === "XXX" || combination === "OOO") setWinner(board[a]);
+    });
+  }
+
+  function resetGame() {
+    setBoard(Array(9).fill(null));
+    setCurrentPlayer("X");
+    setWinner(null);
+  }
+
+  function checkDraw() {
+    if (board.every((value) => value !== null) && winner === null) setWinner("draw");
+  }
+
+  useEffect(() => {
+    checkDraw();
+    checkHasWinner();
+  }, [board]);
+
+  useEffect(() => {
+    if (winner) {
+      console.log(winner === "draw" ? "Draw" : `Player ${winner} won!`);
+      resetGame();
+    }
+  }, [winner]);
 
   return (
     <div className="board">
